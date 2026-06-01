badbd4c52b31b3731171a954e3ea57489feb43e3
pab99 <pab999@gmail.com>
2026-06-01 20:20:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ if(!fs.existsSync(PHOTOS)){
 
 const supabase = createClient(
   'https://tczmjvcqgscpeyivpbyd.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjem1qdmNxZ3NjcGV5aXZwYnlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzNDMzMTAsImV4cCI6MjA5NTkxOTMxMH0.L0nsxcqFzk4F8FZyVy7ogPmet7bQePDHn-bbTdSaZ0k'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjem1qdmNxZ3NjcGV5aXZwYnlkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDM0MzMxMCwiZXhwIjoyMDk1OTE5MzEwfQ.JkfuUD6RCNM2BVg6t2jgfK5dJxTGLeMarBn2UXzA_aw
+'
 );
 
 const MIME = {
